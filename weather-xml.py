df1678a60f094b69c37eb0a5f551b930665262b6
EmiliaAlvarenga <emilia.alvarenga@outlook.es>
2014-03-17 09:41:56 +0000

    Update weather-xml.py

--- weather-xml.py
+++ weather-xml.py
@@ -7,98 +7,22 @@ import webbrowser
 
 #funcion que devuelve la orientacion del viento norte N,nordeste NE,este E, ......
 def direccion_vi(cadena):
-	if cadena>=337.5 and cadena<=360.0 or cadena>=0 and cadena<22.5:
-		return "N"
-	if cadena>=22.5 and cadena<=67.5:
-		return "NE"
-	if cadena>=67.5 and cadena<112.5:
-		return "E"
-	if cadena>=112.5 and cadena<157.5:
-		return "SE"
-	if cadena>=157.5 and cadena<202.5:
-		return "S"
-	if cadena>=202.5 and cadena<247.5:
-		return "SE" 
-	if cadena>=247.5 and cadena <292.5:
-		return "O"
-	if cadena>=292.5 and cadena<337.5:
-		return "NO"
-
-f=open('plantilla1.html','r')
-f_weather=open('weather.html','w')
-html=''
-temp_min =[]
-temp_max = []
-velocidad_viento = []
-direccion = []
-provincia_salida=[]
-
-dicc_ciudad={'1':'Almeria','2':'Cadiz','3':'Cordoba','4':'Granada','5':'Huelva','6':'Jaen','7':'Malaga','8':'Sevilla'}
-provincias=dicc_ciudad.values()
-url="http://api.openweathermap.org/data/2.5/weather?"
-#!/usr/bin/env python
-
-from lxml import etree
-import requests
-from jinja2 import Template
-import webbrowser
-
-#funcion que devuelve la orientacion del viento norte N,nordeste NE,este E, ......
-def direccion_vi(cadena):
-	if cadena>=337.5 and cadena<=360.0 or cadena>=0 and cadena<22.5:
-		return "N"
-	if cadena>=22.5 and cadena<=67.5:
-		return "NE"
-	if cadena>=67.5 and cadena<112.5:
-		return "E"
-	if cadena>=112.5 and cadena<157.5:
-		return "SE"
-	if cadena>=157.5 and cadena<202.5:
-		return "S"
-	if cadena>=202.5 and cadena<247.5:
-		return "SE" 
-	if cadena>=247.5 and cadena <292.5:
-		return "O"
-	if cadena>=292.5 and cadena<337.5:
-		return "NO"
-
-f=open('plantilla1.html','r')
-f_weather=open('weather.html','w')
-html=''
-temp_min =[]
-temp_max = []
-velocidad_viento = []
-direccion = []
-provincia_salida=[]
-
-dicc_ciudad={'1':'Almeria','2':'Cadiz','3':'Cordoba','4':'Granada','5':'Huelva','6':'Jaen','7':'Malaga','8':'Sevilla'}
-provincias=dicc_ciudad.values()
-url="http://api.openweathermap.org/data/2.5/weather?"
-#!/usr/bin/env python
-
-from lxml import etree
-import requests
-from jinja2 import Template
-import webbrowser
-
-#funcion que devuelve la orientacion del viento norte N,nordeste NE,este E, ......
-def direccion_vi(cadena):
-	if cadena>=337.5 and cadena<=360.0 or cadena>=0 and cadena<22.5:
-		return "N"
-	if cadena>=22.5 and cadena<=67.5:
-		return "NE"
-	if cadena>=67.5 and cadena<112.5:
-		return "E"
-	if cadena>=112.5 and cadena<157.5:
-		return "SE"
-	if cadena>=157.5 and cadena<202.5:
-		return "S"
-	if cadena>=202.5 and cadena<247.5:
-		return "SE" 
-	if cadena>=247.5 and cadena <292.5:
-		return "O"
-	if cadena>=292.5 and cadena<337.5:
-		return "NO"
+    if cadena>=337.5 and cadena<=360.0 or cadena>=0 and cadena<22.5:
+       	return "N"
+    if cadena>=22.5 and cadena<=67.5:
+       	return "NE"
+    if cadena>=67.5 and cadena<112.5:
+       	return "E"
+    if cadena>=112.5 and cadena<157.5:
+        return "SE"
+    if cadena>=157.5 and cadena<202.5:
+        return "S"
+    if cadena>=202.5 and cadena<247.5:
+        return "SE" 
+    if cadena>=247.5 and cadena <292.5:
+        return "O"
+    if cadena>=292.5 and cadena<337.5:
+        return "NO"
 
 f=open('plantilla1.html','r')
 f_weather=open('weather.html','w')
